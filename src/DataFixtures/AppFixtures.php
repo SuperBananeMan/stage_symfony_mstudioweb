f84fcb046e18d0a12e3d9fb6136aef6616024ca2
SuperBananeMan <damien.lolz.destructor@gmail.com>
2023-07-13 12:27:29 +0200

    Ajout fonctionnalite et debut ajout base de donne auto

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\VinylMix;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,16 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $mix = new VinylMix();
+		$titles = ['Do you Remember... Phil Collins?!','I like trains','Professionals have standarts.'];
+        $mix->setTitle($titles[array_rand($titles)]);
+		$descriptions = ['A pure mix of drummers turned singers!','I have no idea','Hi this is Patrick',"The engineer's a bloody sentry ! OHOHOHOHOHOHOH !"];
+        $mix->setDescription($descriptions[array_rand($descriptions)]);
+        $genres = ['pop','rock','metal','rap'];
+		$mix->setGenre($genres[array_rand($genres)]);
+        $mix->setTrackCount(rand(5, 20));
+        $mix->setVotes(rand(0, 100));
+		$manager->persist($mix);
 
         $manager->flush();
     }
