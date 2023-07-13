7efad746afe3d9688cc941355ac5f6ba65cd7c82
SuperBananeMan <damien.lolz.destructor@gmail.com>
2023-07-13 12:38:17 +0200

    auto db

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,21 +5,13 @@ namespace App\DataFixtures;
 use App\Entity\VinylMix;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Factory\VinylMixFactory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $mix = new VinylMix();
-		$titles = ['Do you Remember... Phil Collins?!','I like trains','Professionals have standarts.'];
-        $mix->setTitle($titles[array_rand($titles)]);
-		$descriptions = ['A pure mix of drummers turned singers!','I have no idea','Hi this is Patrick',"The engineer's a bloody sentry ! OHOHOHOHOHOHOH !"];
-        $mix->setDescription($descriptions[array_rand($descriptions)]);
-        $genres = ['pop','rock','metal','rap'];
-		$mix->setGenre($genres[array_rand($genres)]);
-        $mix->setTrackCount(rand(5, 20));
-        $mix->setVotes(rand(0, 100));
-		$manager->persist($mix);
+        VinylMixFactory::createMany(50);
 
         $manager->flush();
     }
